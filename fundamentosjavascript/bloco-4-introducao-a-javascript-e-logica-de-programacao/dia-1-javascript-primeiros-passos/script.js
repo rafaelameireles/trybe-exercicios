c148ac7639efb82ec6e7ad4d47319e044db9fd1d
Rafaela Meireles <rafaella.meireles@hotmail.com>
2022-06-21 19:27:09 -0300

    primeiros operadores matematicos

---
+++ fundamentosjavascript/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/script.js
@@ -0,0 +1,17 @@
+/* //Adição (a + b)
+Subtração (a - b)
+Multiplicação (a * b)
+Divisão (a / b)
+Módulo (a % b)//
+ */
+
+
+let a = 10;
+let b = 5;
+
+console.log (a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(a % b);
+
